d2cd0b356dc95f7aaa089d49ad380ee43ce950a0
Md. Manir Uddin <manir22sust@gmail.com>
2022-09-25 15:30:06 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'reactive-forms';
+
+  myForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    body: new FormControl('', Validators.required),
+  });
+
+  get f() {
+    return this.myForm.controls;
+  }
+  submit() {
+    console.log(this.myForm.value);
+  }
 }
